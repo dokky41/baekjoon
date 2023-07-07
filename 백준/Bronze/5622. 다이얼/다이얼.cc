e333ce28b755eaa3a24207839976165d1ab10667
Dokky <107966608+dokky41@users.noreply.github.com>
2023-07-07 21:17:15 +0900

    [Bronze II] Title: 다이얼, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    char a=' ';
+    int time=0;
+
+    cin >> s;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        a = s[i];
+        switch (a)
+        {
+        case 'A': case 'B': case 'C':
+            time += 3;
+            break;
+        case 'D': case 'E': case 'F':
+            time += 4;
+            break;
+        case 'G': case 'H': case 'I':
+            time += 5;
+            break;
+        case 'J': case 'K': case 'L':
+            time += 6;
+            break;
+        case 'M': case 'N': case 'O':
+            time += 7;
+            break;
+        case 'P': case 'Q': case 'R': case 'S':
+            time += 8;
+            break;
+        case 'T': case 'U': case 'V':
+            time += 9;
+            break;
+        case 'W': case 'X': case 'Y': case 'Z':
+            time += 10;
+            break;
+
+        default:
+            break;
+        }
+
+    }
+
+    std::cout << time;
+
+
+
+    return 0;
+}
