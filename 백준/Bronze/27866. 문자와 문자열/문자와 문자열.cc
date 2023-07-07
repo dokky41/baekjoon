da8d02d51c8430e2b4613e25eebc719caad67667
Dokky <107966608+dokky41@users.noreply.github.com>
2023-07-07 11:10:45 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	string s;
+	int a;
+
+	cin >> s; //문자
+	cin >> a; //인덱스
+
+	cout << s[a-1] << endl;
+	
+
+
+
+
+	return 0;
+}
