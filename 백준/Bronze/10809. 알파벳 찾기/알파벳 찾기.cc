8bfc783181ef3b944592b07ade515b426a13468a
Dokky <107966608+dokky41@users.noreply.github.com>
2023-07-07 16:10:23 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	string s;
+
+	cin >> s;
+	
+	for (int i = 97; i <= 122; i++)
+	{
+		cout << (int)s.find(i) << " ";
+
+	}
+
+
+	return 0;
+}
