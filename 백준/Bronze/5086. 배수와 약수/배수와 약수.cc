5a4ed115cf58260aeec4076c402954b2c1113f6e
Dokky <107966608+dokky41@users.noreply.github.com>
2023-07-12 14:01:35 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
- 
-int main(int argc, char *argv[])
+
+int main() 
 {
 	int x, y;
- 
 	while (1) {
+
 		cin >> x >> y;
- 
-		if (x == 0 && y == 0) {
+
+		if (x == 0 && y == 0)
+		{
 			break;
 		}
- 
-		if (y % x == 0) {
+
+		if (y % x == 0)
+		{
 			cout << "factor" << endl;
 		}
-		else if (x % y == 0) {
+		else if (x % y == 0)
+		{
 			cout << "multiple" << endl;
 		}
-		else {
+		else
+		{
 			cout << "neither" << endl;
 		}
+
 	}
- 
-	return 0;
 }
